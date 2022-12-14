0985bf1bff6c11a7879dee4cdf4e82807ec94cfe
Micaela Matta <31009265+micaela-matta@users.noreply.github.com>
2022-12-14 15:22:49 +0000

    Update fragments_io.py

--- fragments_io.py
+++ fragments_io.py
@@ -14,30 +14,35 @@ from itertools import combinations
 from typing import List
 
 
-def fragment_dictionary(name_of_smi_file):
-    #loads the smile file
-    smile_list = np.genfromtxt(f'{name_of_smi_file}.smi',dtype='str')
+def read_fragments(name_of_smi_file):
+    """
+    reads fragment SMILES from file
+    returns a dictionary of fragments
+    """
+    smile_list = np.genfromtxt(name_of_smi_file, dtype='str')
     #creates a dictionary of fragments
     fragment_dic = { i : d for i, d in enumerate(smile_list)}
     return fragment_dic
 
-def combined_fragments_dic(dic):
-    # Get all SMILES for all possible combinations (of 2 fragments) of the fragment list
-    #All the combinations of dimers smiles from fragment dictionary
-    combo_smiles = (f'{v1}.{v2}'.replace('(*)', '9')
+def make_dimers_dic(fragment_dic):
+    """
+    Reads fragment dictionary
+    generates SMILES for all possible dimers 
+    returns dictionary of dimers
+    """
+    dimer_smiles = (f'{v1}.{v2}'.replace('(*)', '9')
                     for v1, v2 in itertools.combinations(dic.values(), r=2))
     #All the combinations of dimer names from fragment dictionary
-    combo_names = [(f'{k1}-{k2}')
+    dimer_names = [(f'{k1}-{k2}')
                     for k1, k2 in itertools.combinations(dic.keys(), r=2)]
 
-        # Convert all SMILES to mol objects
-    #mols = [Chem.MolFromSmiles(smi) for smi in combo_smiles]
+    # Convert all SMILES to mol objects
+    #mols = [Chem.MolFromSmiles(smi) for smi in dimer_smiles]
 
     #relating the dimer names and dimer smiles back to one another
-    combo_dic = { k : v for k, v in zip(combo_names, combo_smiles) }
+    dimer_dic = { k : v for k, v in zip(dimer_names, dimer_smiles) }
 
     return combo_dic
-    #Creates a list of all the possible combinations as RDKit info
 
 def combined_fragments_dic_mol(dic):
     # Get all SMILES for all possible combinations (of 2 fragments) of the fragment list
