384e31b9b8eb75f73628c4d9af4352f6c5b09ff6
Micaela Matta <31009265+micaela-matta@users.noreply.github.com>
2023-04-09 17:35:23 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+# this can be more complicated and clever, but for now it's minimal
+setup(
+    name='qcflow',
+    version='0.1',
+    packages=find_packages(),
+)
