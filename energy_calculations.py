a9daae82554089e97757e3736d2d06e000cb57fc
“HongdingZhang16” <hongding0116@gmail.com>
2023-04-10 20:08:11 +0100

    all_frag2p.smi and .json update. energy_calculations.py updated for CM5 calculation

--- energy_calculations.py
+++ energy_calculations.py
@@ -1,4 +1,5 @@
 import cclib
+import numpy as np
 from load_gaussian import *
 
 def log_data(name,job):
@@ -32,36 +33,37 @@ def cal_reorg(pop_opt_n,ver,opt,neutral_ion_geo):
 
     return reorg_en
 
-def get_HOMO(data):
+def cal_HOMO(pop_opt_n):
     '''
-    when provided a cclib object, returns the HOMO energy (eV)
+    when provided the cclb object for the optimised neutral population, function returns the HOMO energy(eV).
 
-    data: cclib object  
+    pop_opt_n: cclib obj, cclib parsed .log file for neutral population optimisation analaysis.
     '''
     HOMO = pop_opt_n.moenergies[0][pop_opt_n.homos[0]]
     return HOMO
 
-def get_LUMO(data):
+def cal_LUMO(pop_opt_n):
     '''
-    when provided a cclib object, returns the LUMO energy (eV)
+    when provided the cclb object for the optimised neutral population, function returns the LUMO energy(eV).
 
-    data: cclib object 
+    pop_opt_n: cclib obj, cclib parsed .log file for neutral population optimisation analaysis.
     '''
     LUMO = pop_opt_n.moenergies[0][pop_opt_n.homos[0]+1]
     return LUMO
 
-def calc_HL_gap(data):
+def cal_gap(pop_opt_n):
     '''
-    returns the energy difference between HOMO and LUMO (i.e. HOMO-LUMO gap) in eV.
+    when provided the cclb object for the optimised neutral population, function returns the energy
+    difference between HOMO and LUMO (i.e. HOMO-LUMO gap) in eV.
 
-    data: cclib object
+    pop_opt_n: cclib obj, cclib parsed .log file for neutral population optimisation analaysis.
 
     '''
-    HL_gap = get_LUMO(data) - get_HOMO(data)
+    gap = cal_LUMO(pop_opt_n) - cal_HOMO(pop_opt_n)
 
-    return HL_gap
+    return gap
 
-def calc_IP(pop_opt_n,ver_c,opt_c, kind):
+def cal_IP(pop_opt_n,ver_c,opt_c,kind):
     '''
     when provided the cclb object for the optimised neutral population,vertical cation, optimised cation
     and the type of the IP, function returns the ionisation energy.
@@ -86,7 +88,7 @@ def calc_IP(pop_opt_n,ver_c,opt_c, kind):
 
     return IP
 
-def calc_EA(pop_opt_n, opt_a, an_geo_n, kind):
+def cal_EA(pop_opt_n,opt_a,an_geo_n,kind):
     '''
     when provided the optimised neutral population,optimised anion, neutral anion geometry and
     the type of the EA, function returns the energy for Electron Affinity.
@@ -111,38 +113,60 @@ def calc_EA(pop_opt_n, opt_a, an_geo_n, kind):
 
     return EA
 
-def calc_CSI_dimer(data, dimer_mol, a_mol, b_mol):
+def parse_hirshfeld_charges(dimer):
+    '''when provided the name of the dimer, the function return a list containing
+     CM5 charges, where the first value represents the first atom in the molecule.
+
+     dimer: str, name of the dimer
+    '''
+    g16_out = f'{dimer}/{dimer}_pop_opt_n.log' # change this however you need
+
+    charges_cm5 =  []
+
+    with open(g16_out, 'r') as file_object:
+        for line in file_object:
+            if not (line.startswith(' Hirshfeld charges with hydrogens summed into heavy atoms:')):
+                continue
+            else:
+                next(file_object)
+                line = next(file_object)
+                while len(line.strip()) != 0:
+                    charges_cm5.append(float(line.split()[3]))
+                    line = next(file_object)
+                break
+    return charges_cm5
+
+def cal_CSI(dimer_name,dimer_dic,mel_dic,all_frag_dic):
     '''
     when provided the dimer name, the optimised neutral population energy calculation, dimer dictionary,
     dictionary containing all melanin fragments and the dictionary of all organic electronic fragements,
     function returns the the charge seperation index between the fragments within the oligomer in eV.
 
-    
-    data: cclib obj, cclib parsed .log file for neutral population optimisation analaysis
-    
-    #dimer_name: str, name of the dimer in the dimer dictionary (e.g.'a_2')
-    #dimer_dic: str, name of the .json file containing all dimers
-    #mel_dic: str, name of the .json file containing all the melanin fragments
-    #all_frag_dic: str, name of the .json file containing all the organic electronic fragements
-    
+    dimer_name: str, name of the dimer in the dimer dictionary (e.g.'a_2')
+    pop_opt_n: cclib obj, cclib parsed .log file for neutral population optimisation analaysis
+    dimer_dic: str, name of the .json file containing all dimers
+    mel_dic: str, name of the .json file containing all the melanin fragments
+    all_frag_dic: str, name of the .json file containing all the organic electronic fragements
+
+    use: >charge_a, CSI = cal_CSI()<
 
     '''
-    #dimer =  open_dictionary(dimer_dic)[dimer_name]   #when this is operational, the dictionary file should be mel_frag.json
-    #dimer_mol = rdkit.Chem.MolFromSmiles(dimer.replace('{}',''))
+    dimer =  open_dictionary(dimer_dic)[dimer_name]   #when this is operational, the dictionary file should be mel_frag.json
+    dimer_mol = rdkit.Chem.MolFromSmiles(dimer.replace('{}',''))
 
-    #a = open_dictionary(mel_dic)[dimer_name.split('_')[0]]  #this only works for frag-frag dimer, ideally 'a' should be changed to mel_frag2p.json
-    #a_mol = rdkit.Chem.MolFromSmiles(a.replace('{}',''))
+    a = open_dictionary(mel_dic)[dimer_name.split('_')[0]]  #this only works for frag-frag dimer, ideally 'a' should be changed to mel_frag2p.json
+    a_mol = rdkit.Chem.MolFromSmiles(a.replace('{}',''))
 
-    #b = open_dictionary(all_frag_dic)[dimer_name.split('_')[1]]
-    #b_mol = rdkit.Chem.MolFromSmiles(b.replace('{}',''))
+    b = open_dictionary(all_frag_dic)[dimer_name.split('_')[1]]
+    b_mol = rdkit.Chem.MolFromSmiles(b.replace('{}',''))
 
-    mulliken = data.atomcharges['mulliken']
+    charge = np.array(parse_hirshfeld_charges(dimer_name))
 
-    charge_a = mulliken[list(dimer_mol.GetSubstructMatch(a_mol))].sum()
+    charge_a = charge[list(dimer_mol.GetSubstructMatch(a_mol))].sum()
 
-    charge_b = mulliken[list(dimer_mol.GetSubstructMatch(b_mol))].sum()
+    charge_b = charge[list(dimer_mol.GetSubstructMatch(b_mol))].sum()
 
 
     CSI = ((charge_a - charge_b)**2)**0.5
 
-    return CSI
+    return charge_a, CSI
