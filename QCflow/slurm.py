635c91a47a0f05652f8afca8572b465fa72180ee
Micaela Matta <31009265+micaela-matta@users.noreply.github.com>
2023-10-30 19:06:21 +0000

    Update slurm.py
    modified defaults:
    - loads g16 instead of g09 modules
    - uses 8 cpus per job

--- QCflow/slurm.py
+++ QCflow/slurm.py
@@ -30,13 +30,13 @@ def write_slurm(job_name, mol_name):
     title = f'#!/bin/bash --login'
     with open(file_name, 'w') as file:
         file.write(f'{title}\n')#
-        file.write(f'#SBATCH -o g09_%J.out \n')
-        file.write(f'#SBATCH -e g09_%J.err \n')#
+        file.write(f'#SBATCH -o g16_%J.out \n')
+        file.write(f'#SBATCH -e g16_%J.err \n')#
         file.write(f'#SBATCH --job-name={mol_name}_{job_name} \n')
         file.write(f'#SBATCH -p cpu \n')
-        file.write(f'#SBATCH --ntasks=10 \n')
+        file.write(f'#SBATCH --ntasks=8 \n')
         file.write(f'#SBATCH --nodes=1 \n')
-        file.write(f'#SBATCH --tasks-per-node=10 \n')
+        file.write(f'#SBATCH --tasks-per-node=8 \n')
         file.write(f'#SBATCH --mem-per-cpu=4000 \n')
         file.write(f'#SBATCH --time=48:00:00 \n')
         file.write(' \n')#
@@ -44,19 +44,19 @@ def write_slurm(job_name, mol_name):
         file.write(f'OUTPUTFILE="\$(basename "\$INPUTFILE" .com).log" \n')
         file.write(' \n')
         file.write(f'module purge \n')
-        file.write(f'module load test_switch_kcl/1.0.0-gcc-9.4.0 \n')
-        file.write(f'module load gaussian_sse4_kcl/09-E-gcc-9.4.0 \n')
+        file.write(f'module load test_switch_kcl/1.0.0-gcc-10.3.0 \n')
+        file.write(f'module load gaussian_sse4_kcl/16-C-gcc-10.3.0 \n')
         file.write(f'export GOMP_CPU_AFFINITY=$SGE_BINDING \n')
         file.write(f'export KMP_AFFINITY="explicit,proclist=$SGE_BINDING,verbose" \n')
-        file.write(f'#source $g09root/bsd/g09.login \n')
+        file.write(f'#source $g16root/bsd/g16.login \n')
         file.write(' \n')
-        file.write(f'echo "G09 job \$SLURM_JOBID" \n')
+        file.write(f'echo "G16 job \$SLURM_JOBID" \n')
         file.write(f'echo "INPUT \$INPUTFILE" \n')
         file.write(f'echo "OUTPUT \$OUTPUTFILE" \n')
         file.write(f'echo "Running \$SLURM_NTASKS on \$SLURM_JOB_NODELIST" \n')
         file.write(' \n')
         file.write(f'#Execution Line \n')
-        file.write(f'g09 "$INPUTFILE" > "$OUTPUTFILE" \n')
+        file.write(f'g16 "$INPUTFILE" > "$OUTPUTFILE" \n')
 
 
 def submit_slurm_job(job_name, mol_name):
